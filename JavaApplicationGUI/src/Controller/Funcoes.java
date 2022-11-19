4e8bf6cb3d10c0c538656246c441e9b141cab0c4
Caroline Batista Vantim <82098788+CarolineVantim@users.noreply.github.com>
2022-11-18 22:17:13 -0300

    Aula 18/11

---
+++ JavaApplicationGUI/src/Controller/Funcoes.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author aluno.si
+ */
+public class Funcoes {
+    
+    public int somar(int valor1, int valor2){
+        
+        int resultado = 0;
+        
+        resultado = valor1 + valor2;
+        
+        return resultado;
+    }
+}
